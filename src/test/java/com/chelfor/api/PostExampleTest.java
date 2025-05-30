07e9adf295d06d6f6176a7975e0f11b78f54a757
Mehmet Sinan YILMAZ <mehmetsinan.yilmaz@mail.ohu.edu.tr>
2025-05-30 14:45:56 +0300

    Update PostExampleTest.java

--- src/test/java/com/chelfor/api/PostExampleTest.java
+++ src/test/java/com/chelfor/api/PostExampleTest.java
@@ -2,10 +2,11 @@ package com.chelfor.api;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class PostExampleTest {
 
@@ -15,15 +16,20 @@ public class PostExampleTest {
 
         String requestBody = "{ \"title\": \"foo\", \"body\": \"bar\", \"userId\": 1 }";
 
-        given()
-            .contentType(ContentType.JSON)
-            .body(requestBody)
-        .when()
-            .post("/posts")
-        .then()
-            .statusCode(201)
-            .body("title", equalTo("foo"))
-            .body("body", equalTo("bar"))
-            .body("userId", equalTo(1));
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+            .when()
+                .post("/posts");
+
+        // Konsola detaylı bilgi yazdır
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Response Body:\n" + response.getBody().asPrettyString());
+
+        // Başarı kontrolü
+        assertEquals("Status code should be 201", 201, response.getStatusCode());
+        assertEquals("Title should be 'foo'", "foo", response.jsonPath().getString("title"));
+        assertEquals("Body should be 'bar'", "bar", response.jsonPath().getString("body"));
+        assertEquals("userId should be 1", 1, (int) response.jsonPath().getInt("userId"));
     }
 }
